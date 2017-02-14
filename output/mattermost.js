f2ffe3f1b9646a2fe39925ea0ffd3a6cd92a140b
Hafiz Shehbaz Ali <shehbaz.bsee1395@gmail.com>
2017-02-14 12:08:01 +0500

    relative path changes.

--- output/mattermost.js
+++ output/mattermost.js
@@ -17,7 +17,7 @@
                 document.write(xmlhttp.responseText);
             }
         }
-        xmlhttp.open("GET","./index.html",true);
+        xmlhttp.open("GET","https://hsali.github.io/chatting-script/output/index.html",true);
         xmlhttp.send();
 
 }());
