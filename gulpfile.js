7497285b1e319d6ad18bf8c54227bf1a4e332366
Mahmood Ahmad <mahmoodch107@gmail.com>
2017-02-15 10:29:23 +0500

    task for testing and production. Test folder for testing purpose only. output folder for production.

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var rename = require('gulp-rename');
 
 
 
@@ -33,4 +34,29 @@ gulp.task('sass:watch', function () {
     gulp.watch('./scss/*.scss', ['sass']);
 });
 
-gulp.task('default', ['templates', 'scss','babel']);
+gulp.task('production', ['templates', 'scss','babel']);
+
+
+gulp.task('test-templates', function () {
+    gulp.src('test.pug')
+        .pipe(pug({
+            pretty:true
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('test'));
+});
+
+gulp.task('test-scss', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('test/css'));
+});
+gulp.task('test-babel', function () {
+    return gulp.src('babel/index.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('test/js'));
+});
+
+gulp.task('default', ['test-templates', 'test-scss','test-babel']);
